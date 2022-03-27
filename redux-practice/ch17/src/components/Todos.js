da6711a14f35707f0bd420f97241da5261b24df9
ding-co <pangoons@naver.com>
2022-03-27 16:52:58 +0900

    [ADD] Ch 17 - Redux - React 1

--- redux-practice/ch17/src/components/Todos.js
+++ redux-practice/ch17/src/components/Todos.js
@@ -11,19 +11,25 @@ const Todos = ({
 }) => {
   const onSubmit = (e) => {
     e.preventDefault();
+    onInsert(input);
+    onChangeInput('');
   };
+  const onChange = (e) => onChangeInput(e.target.value);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input />
+        <input value={input} onChange={onChange} />
         <button type="submit">등록</button>
       </form>
       <div>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
       </div>
     </div>
   );

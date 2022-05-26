1aa504d06290ec0f9bc9bf30e222178335ffbe5f
ding-co <pangoons@naver.com>
2022-05-27 07:41:20 +0900

    [ADD] ch 23 - JWT 02

--- code/ch23/src/models/user.js
+++ code/ch23/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -16,7 +17,27 @@ UserSchema.methods.checkPassword = async function (password) {
   return result;
 };
 
-UserSchema.static.findByUsername = function (username) {
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+    },
+  );
+  return token;
+};
+
+UserSchema.statics.findByUsername = function (username) {
   return this.findOne({ username });
 };
 
